ec9e86d90ea4a0f61abbab66962595785bff0dbc
Trevor Jim <tjim@mac.com>
2010-12-06 14:58:06 -0500

    Simplify late position ($pos) desugaring
    File s.bnf:
    
        S = "hello" $pos$x "there" {Printf.printf "%d\n%!" x}
    
    Test run:
    
        $ echo -n 'hellothere' | build/yakker exec s.bnf
        5
    
    Before:
    
        $ build/yakker desugar s.bnf
        S = "hello" (@pos@_x3 @delay(_x3))$x "there" {Printf.printf "%d\n%!" x}.
    
    After:
    
        $ build/yakker desugar s.bnf
        S = "hello" @delay(pos_)$x "there" {Printf.printf "%d\n%!" x}.

--- src/desugar.ml
+++ src/desugar.ml
@@ -25,27 +25,30 @@ let desugar g =
   let rec loop r = match r.r with
     | Lookahead(b, r2) ->
         loop r2;
-	(* If it is a symbol or a character set, we leave it in
-	place. Otherwise, we lift it out. *)
-	(match r2 with
-	   | {r=Symb _}
-	   | {a={css=Some(_)}} -> ()
-	   | _ ->
+        (* If it is a symbol or a character set, we leave it in
+        place. Otherwise, we lift it out. *)
+        (match r2 with
+           | {r=Symb _}
+           | {a={css=Some(_)}} -> ()
+           | _ ->
                r2.r <- define r2)
     | Position false ->
         let x = Variables.fresh() in
         r.r <- (mkSEQ2(mkPOSITION true,Some x,None,mkDELAY(x,None))).r
+(*
+        r.r <- (mkDELAY("pos_",None)).r
+*)
     | Symb _ | Position _ | Prose _
     | When _ | Action _ | Box _ | Delay _
     | CharRange _ | Minus _ | Lit _ -> ()
     | Seq(r2,_,_,r3)
     | Alt(r2,r3) ->
-	loop r2; loop r3
+        loop r2; loop r3
     | Opt(r2)
     | Assign(r2,_,_)
     | Rcount(_,r2)
     | Star(_,r2)
     | Hash(_,r2) ->
-	loop r2
+        loop r2
   in
   List.iter (function RuleDef(_,r,_) -> loop r | _ -> ()) g.ds
